4f27580fe56606ffb0f2f7c98be73f0d80579810
rachid <rachidoumjahdeemail@gmail.com>
2022-09-19 03:38:39 +0100

    make it better

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+  return "<h1>Welcome to CodingX</h1>"
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",threaded=True)
+
+
