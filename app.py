fee74e52e7544774d3fba2b8d657c0af8081cf84
rachid <rachidoumjahdeemail@gmail.com>
2022-09-30 18:06:09 +0100

    make it better

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask ,jsonify
 import requests
+import logging
+import json
 
 
 
 
 app= Flask(__name__)
-#logging.basicConfig(filename='app.log', format='%(asctime)s: %(levelname)s, %(message)s', level=logging.DEBUG)
+logging.basicConfig(filename='app.log', format='%(asctime)s: %(levelname)s, %(message)s', level=logging.DEBUG)
 BASE_URL ="https://mobipolar.online/api/property/lagecy_v1?bracket_key={}"
 
 @app.route('/')
@@ -14,19 +16,34 @@ def index():
 
 
 
-# def readJSONfile(path):
-#   try :
-#    f = open("jsonbackend/"+path)
-#    data = json.load(f)
-#    return data
-#   except Exception as e:
-#     return None  
-
+def readJSONfile(path):
+  try :
+   f = open("jsonbackend/"+path)
+   data = json.load(f)
+   return data
+  except Exception as e:
+    return None  
 
 @app.route('/<path>',methods=['GET'])
 def getapp(path):
-   data = requests.get("https://mobipolar.online")
-   return data.json()
+  try :
+   data = requests.get(BASE_URL.format(path))
+   return data.json()["data"]
+  except Exception as e  :
+    logging.error(e)
+    try :
+      resp = readJSONfile(path)
+      if(resp==None):
+       return jsonify({"message":"not found"}) 
+      else :
+       return readJSONfile(path)
+    except Exception as e:
+      logging.error(e)
+      return jsonify({"message":str(e)}) 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",threaded=True)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",threaded=True)
 
