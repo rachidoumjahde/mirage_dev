527a233bb62b25336d1dc8d9435bc38994baf7b9
rachid <rachidoumjahdeemail@gmail.com>
2022-09-30 17:16:28 +0100

    make it better

--- app.py
+++ app.py
@@ -13,29 +13,6 @@ BASE_URL ="https://mobipolar.online/api/property/lagecy_v1?bracket_key={}"
 def index():
   return "<h1>P</h1>"
 
-
-def loadfromFile(file_name):
- try:
-  f = open('jsonbackend/'+file_name)
-  data = json.load(f)
-  return data
- except Exception as e:
-    return None
-
-
-@app.route('/<path>',methods=['GET'])
-def getSettigns(path):
-    db_data = requests.get(BASE_URL.format(path))
-    try :
-     return db_data.json()["data"]
-    except Exception as e :
-     resp = loadfromFile(path)   
-     if(resp==None):
-        return resp
-     else :
-      return loadfromFile(path)
-        
-        
 if __name__ == "__main__":
     app.run(host="0.0.0.0",threaded=True)
 
